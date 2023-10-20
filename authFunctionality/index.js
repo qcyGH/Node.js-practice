5852adde1c79978a45c99983926291d73194d7e9
qcy <12maks32@gmail.com>
2023-10-20 15:54:47 +0200

    Made connection to db

--- authFunctionality/index.js
+++ authFunctionality/index.js
@@ -1,10 +1,18 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+
+dotenv.config()
+
 const PORT = process.env.PORT || 5000
+const DATABASE_USER = process.env.DATABASE_USER
+const DATABASE_PASSWORD = process.env.DATABASE_PASSWORD
 
 const app = express()
 
-const start = () => {
+const start = async () => {
   try {
+    await mongoose.connect(`mongodb+srv://${DATABASE_USER}:${DATABASE_PASSWORD}@maincluster.e5okmwd.mongodb.net/?retryWrites=true&w=majority`)
     app.listen(PORT, () => console.log(`Server has been started on port ${PORT}`))
   } catch(e) {
     console.error(e)
