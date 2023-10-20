18aba1c9b5831e43ac370b85101fc9d676235226
qcy <12maks32@gmail.com>
2023-10-20 16:54:49 +0200

    Made class authController

---
+++ authFunctionality/authController.js
@@ -0,0 +1,28 @@
+class authController {
+
+  async registration (req, res) {
+    try {
+
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  async login (req, res) {
+    try {
+
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  async getUsers (req, res) {
+    try {
+
+    } catch (error) {
+      console.error(error)
+    }
+  }
+}
+
+module.exports = new authController()
--- authFunctionality/index.js
+++ authFunctionality/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const authRouter = require('./authRouter')
+const authController = require('./authController')
 
 dotenv.config()
 
