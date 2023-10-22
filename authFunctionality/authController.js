1629a2b80800a0eecce76cae92fa5b3b1f32f59d
qcy <12maks32@gmail.com>
2023-10-22 23:02:36 +0200

    Made registration method

--- authFunctionality/authController.js
+++ authFunctionality/authController.js
@@ -1,12 +1,27 @@
 const User = require('./models/User')
 const Role = require('./models/Role')
+const bcrypt = require ('bcrypt')
 
 class authController {
 
   async registration (req, res) {
     try {
+      const { username, password } = req.body
+
+      const candidate = await User.findOne({username})
+      if (candidate) {
+        return res.status(400).json('This username is already taken')
+      }
+
+      const hashPassword = bcrypt.hashSync(password, 7)
+      const userRole = await Role.findOne({value: "USER"})
+      const user = new User({username, password: hashPassword, roles: [userRole.value]})
+      await user.save()
+
+      return res.json({message: 'User successfully registered'})
 
     } catch (error) {
+
       console.error(error)
       res.status(400).json({message: 'Registration error'})
     }
@@ -16,6 +31,7 @@ class authController {
     try {
 
     } catch (error) {
+
       console.error(error)
       res.status(400).json({message: 'Login error'})
     }
@@ -26,6 +42,7 @@ class authController {
 
       res.json('Server work')
     } catch (error) {
+
       console.error(error)
       res.status(400).json({message: 'Get users error'})
     }
