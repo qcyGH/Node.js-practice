059aecaed53a226a5885e8e904c32c7dcf38e7b3
qcy <12maks32@gmail.com>
2023-10-23 13:14:00 +0200

    Added express-validator to project and made validation for registration

--- authFunctionality/authRouter.js
+++ authFunctionality/authRouter.js
@@ -1,8 +1,12 @@
 const Router = require('express')
 const router = new Router()
 const controller = require('./authController')
+const { check } = require('express-validator')
 
-router.post('/registration', controller.registration)
+router.post('/registration', [
+  check('username', 'Username must be filled').notEmpty(),
+  check('password', 'Password length must be between 4 and 24').isLength({min: 4, max: 24})
+], controller.registration)
 router.post('/login', controller.login)
 router.get('/users', controller.getUsers)
 
