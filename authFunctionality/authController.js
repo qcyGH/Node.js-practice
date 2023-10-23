b2a92d5f5a1417fe7f8f46986ef5342dabd0a09f
qcy <12maks32@gmail.com>
2023-10-23 18:03:37 +0200

    Added validationResult condtion

--- authFunctionality/authController.js
+++ authFunctionality/authController.js
@@ -1,11 +1,17 @@
 const User = require('./models/User')
 const Role = require('./models/Role')
 const bcrypt = require ('bcrypt')
+const { validationResult } = require('express-validator')
 
 class authController {
 
   async registration (req, res) {
     try {
+      const errors = validationResult(req)
+      if (!errors.isEmpty()) {
+        return res.status(400).json({message: 'Registration error'})
+      }
+
       const { username, password } = req.body
 
       const candidate = await User.findOne({username})
