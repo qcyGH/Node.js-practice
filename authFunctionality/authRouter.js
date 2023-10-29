a45001fb194840fb1b03044a44cda9cf0cb4cce8
qcy <12maks32@gmail.com>
2023-10-29 17:13:32 +0100

    Connect authMiddleware to users router

--- authFunctionality/authRouter.js
+++ authFunctionality/authRouter.js
@@ -2,12 +2,13 @@ const Router = require('express')
 const router = new Router()
 const controller = require('./authController')
 const { check } = require('express-validator')
+const authMiddleware = require('./middleware/authMiddleware')
 
 router.post('/registration', [
   check('username', 'Username must be filled').notEmpty(),
   check('password', 'Password length must be between 4 and 24').isLength({min: 4, max: 24})
 ], controller.registration)
 router.post('/login', controller.login)
-router.get('/users', controller.getUsers)
+router.get('/users', authMiddleware, controller.getUsers)
 
 module.exports = router
